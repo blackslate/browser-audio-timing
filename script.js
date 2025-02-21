20657350313aa1294e7a407463d5df2c52203213
James Newton <blackslate@lexogram.com>
2025-02-22 01:50:11 +0300

    Enable selction of Opera; add treat next/prev buttons
    timings.json
    ---
    * Fix certain firefox times
    
    index.html
    ---
    * Add scroll section to tweak buttons, with up and down
    
    script.js
    ---
    * Add comprehensive comments
    * Add hard-coded constants for red/blue shift and
      play-mark
    * Fix userAgent detection for Opera
    * Add "active" class to timing spans in first column
    * Remove unused classes from #predefined timing spans
    * Adjust getShift() to exaggerate bg-color for slight
      deltas
    * Fix position of play-mark when #predefined is scrolled
    * Add tweakNext() function to handle up|down navigation
    
    styles.css
    ---
    * Tweak and reorganize CSS to simplify it
    * Reduce use of width in timing spans

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
  */
 
 
+// Load timings from external JSON file
 fetch('./timings.json')
   .then(response => response.json())
   .then(json => json.words || json)
@@ -10,11 +11,30 @@ fetch('./timings.json')
   .catch(error => console.log("error:", error))
 
 
+// <<< HARD-CODED constants
 const DELAY = 2500
 
-// Possibly inaccurate check on userAgent
-const userAgent    = (/chrome|safari|edge|firefox|opera/i.exec(navigator.userAgent) || ["chrome"])[0].toLowerCase()
+// Calculating red/blue shift
+const MAX   = 1.5  // max observed = 1.4 for 182 on Firefox
+const PEAK  = 255  // "FF"
+const N     = 0.5  // sqrt exaggerates color at lower deltas
+const SCALE = Math.pow(PEAK, 1/N) / MAX
 
+// Calculating top of play-mark div for Play All First Column
+const FONT_SIZE = 16
+const RATIO     = 2.4
+const JUMP      = FONT_SIZE * RATIO
+// HARD-CODED >>>
+
+
+// Possibly inaccurate check on userAgent
+const userAgent = 
+  navigator.userAgent.match(/Opera|OPR\//i)
+  ? "opera"
+  : (/opr|chrome|safari|edge|firefox|opera/i.exec(navigator.userAgent) || ["chrome"])[0].toLowerCase()
+
+
+// Get pointers to interactive DOM elements
 const selectFile = document.getElementById("select-file")
 const first      = document.getElementById("first")
 const second     = document.getElementById("second")
@@ -29,27 +49,32 @@ const start      = document.getElementById("start")
 const finish     = document.getElementById("finish")
 const playOne    = document.getElementById("play-one")
 const set        = document.getElementById("set")
+
+const scroll     = document.getElementById("scroll")
 const logTiming  = document.getElementById("log-timing")
 
 
-const settings = {
-  offset: 0,
-  scale: 1,
-  stretch: 1
-}
-
-let filesData      // { key: { url: "", word: { clip }}, ...}
-let url            // file containing audio
-let timing = []    // [ { label, clip: [ start, finish ]}, ... ]
-let timings = []   // [ [ start, finish ], ... ] for Play All
-let index          // of item in timing that was clicked
-let clip = [0, 0]  // [ start, finish ] of clicked item in timing
-let interval       // integer value from clearInterval
-
+// Adopt detected browser in first select element
 first.value = userAgent
 let using = first.value
 
+let filesData     // { key: { url: "", word: { clip }}, ...}
+let url           // file containing audio
+let timing = []   // [ "url": "./audio.file",
+                  //   { label,
+                  //     clip: [ start, finish ],
+                  //     <browser>: [ start, finish ] },
+                  //     ...
+                  //   },
+                  //   ...
+                  // ]
+let timings = []  // [ [ start, finish ], ... ] for Play All
+let index = 1     // of item in timing that was chosen
+let clip = [0, 0] // [ start, finish ] of clicked item in timing
+let interval      // integer value from clearInterval
 
+
+// Event listeners
 selectFile.addEventListener("change", changeFile)
 first.addEventListener("change", setComparison)
 second.addEventListener("change", setComparison)
@@ -61,15 +86,24 @@ playOne.addEventListener("click", playCustomClip)
 start.addEventListener("change", updateClip)
 finish.addEventListener("change", updateClip)
 set.addEventListener("click", updateSettings)
+
+scroll.addEventListener("click", tweakNext)
 logTiming.addEventListener("click", logTimingData)
 
 
+/**
+ * Called when fetch promise is resolved.
+ * 
+ * @param {object} json was read from timings.json
+ * 
+ * Create option for the select menu to choose which file to treat
+ */ 
 function initialize(json) {
   filesData = json
 
-  // Create a select menu to choose which file to treat
   const keys = Object.keys(json)
   if (keys.length > 1) {
+    // Multiple options
     const options = keys.forEach( key => {
       const option = document.createElement("option")
       option.value = key
@@ -78,20 +112,40 @@ function initialize(json) {
     })
 
   } else {
+    // Only one file defined in timings.json: use span, not select
     const nameSpan = document.createElement("span")
     const data = filesData[keys[0]]
     nameSpan.innerText = data.url.replace(/^[./]+/, "")
     selectFile.replaceWith(nameSpan)
   }
 
+  // Load the first file defined in timings.json
   changeFile({ target: { value: keys[0] }})
 }
 
 
+/**
+ * Called when the selectFile option is changed. Loads the data
+ * associated with the new audio file.
+ * 
+ * Creates the timing object, with the format...
+ * 
+ *   [ "url": "./audio.file",
+ *     { label,
+ *       "clip": [ start, finish ],
+ *       <browser>: [ start, finish ] },
+ *       ...
+ *     },
+ *     ...
+ *   ]
+ * 
+ * ... and generates a DOM list to display the snippets
+ */
 function changeFile({ target }) {
   const file = target.value
   timing = Object.entries(filesData[file])
   url = timing[0][1]
+
   timing = timing.map(([ label, timing ] ) => {
     if (typeof timing === "object") {
       return { label, ...timing }
@@ -104,6 +158,13 @@ function changeFile({ target }) {
 }
 
 
+/**
+ * Called by either the first or second comparison selector
+ * 
+ * @param {object} onChange event
+ * 
+ * Regenerates the timing list for the requested browser or source
+ */
 function setComparison({ target }) {
   const { id, value } = target
 
@@ -115,6 +176,20 @@ function setComparison({ target }) {
 }
 
 
+/**
+ * Called by changeFile() and setComparison(), when the audio
+ * file or the browser to compare changes
+ * 
+ * Generates a list of interactive items, each with three parts:
+ *           
+ *            first comparison     second comparison
+ *   -----    -----------------    -----------------
+ *   label    [ start, finish ]    [ start, finish ]
+ * 
+ * The start-finish buttons may be coloured blue or red depending
+ * on whether the timing of the playback requestsneeds to be
+ * earlier or later than the audio file's actual timing. 
+ */
 function generateList() {
   while (predefined.firstChild) {
     predefined.removeChild(predefined.lastChild);
@@ -137,8 +212,8 @@ function generateList() {
       const mid = parseInt((start + finish) * 500, 10) / 1000
 
       // Create two columns for comparison
-      ;[left, right].forEach( side => {
-        const times = getTimes(side, timingData, mid)
+      ;[left, right].forEach(( side, index )=> {
+        const times = getTimes(side, timingData, mid, !index)
         line.append(times)
       })
 
@@ -148,7 +223,28 @@ function generateList() {
 }
 
 
-function getTimes(side, timingData, mid) {
+/**
+ * Called by generateList() and updateSettings()
+ * @param {string} side:      "clip" | "chrome" | <browser>
+ * @param {array} timingData: [ start, finish ] (seconds)
+ * @param {number} mid:       actual middle of clip in audio file
+ * @param {boolean} isActive: true if element is for first column
+ *  
+ * @returns a span element with a structure like...
+ * 
+ * <span
+ *   class="firefox button active"
+ *   style="background-color: rgb(32, 0, 0);"
+ * >
+ *   <span>0.95</span>
+ *   <span>-</span>
+ *   <span>2.15</span>
+ * </span>
+ * 
+ * The background-color indicates if the timing request should be
+ * earlier or later than the audio file's own data expects.
+ */
+function getTimes(side, timingData, mid, isActive) {
   const [ start, finish ] = timingData[side]
   // Determine the red/blue shift with respect to expected
   const centre = parseInt((start + finish) * 500, 10) / 1000
@@ -156,16 +252,17 @@ function getTimes(side, timingData, mid) {
 
   const times = document.createElement("span")
   times.classList.add(side, "button")
+  if (isActive) {
+    times.classList.add("active")
+  }
+  
   times.style.backgroundColor = bg
 
   const begin = document.createElement("span")
-  begin.classList.add("begin")
   begin.innerText = start.toFixed(2)
   const to = document.createElement("span")
-  to.classList.add("to")
   to.innerText = "-"
   const end = document.createElement("span")
-  end.classList.add("end")
   end.innerText = finish.toFixed(2)
 
   times.append(begin)
@@ -177,24 +274,49 @@ function getTimes(side, timingData, mid) {
 
 
 
+/**
+ * Called by getTimes()
+ * @param {number} delta will be the number of seconds (±) 
+ *                 differences between the expected clip timing
+ *                 and the effective timing for this browser
+ * @returns a color in the format "#rr00bb", where either rr or bb
+ *                 will be "00", and the other not, or possibly
+ *                 "#222" if delta is zero.
+ */
 function getShift(delta) {
-  let r = 0,
-      b = 0
+  let r = "00",
+      b = "00"
 
-  if (delta < 0) { // min -1.4 => f
-    b = Math.round(-11 * delta).toString(16)
-  } else if (delta > 0) { // 0.625 => 7
-    r = Math.round(11 * delta).toString(16)
+  if (delta < 0) {        // min -1.4 => "ff"
+    b = hex(-delta)
+  } else if (delta > 0) { // max 0.625 => "aa"
+    r = hex(delta)
   }
 
-  const shift = (r || b)
-    ? `#${r}0${b}`
+  const shift = (delta)
+    ? `#${r}00${b}`
     : "#222"
 
   return shift
+
+  function hex(dec) {
+    dec = Math.round(Math.pow(dec * SCALE, N)) // 0 - 255
+    return (dec < 16
+      ? "0"
+      : ""
+    )
+    + dec.toString(16) // "00" - "ff"
+  }
 }
 
 
+/**
+ * Called by a click even on the #predefined list or by 
+ * tweakNext()
+ * 
+ * @param {target} will be the element in an `li` item that
+ * was selected.
+ */ 
 function selectTime({ target }) {
   const line = target.closest("li")
   index = Array.from(predefined.children).indexOf(line) + 1
@@ -205,8 +327,10 @@ function selectTime({ target }) {
 
   } else {
     const column = target.closest(".button")
-    // Assume className only contains "button <key>"
-    const key = column.className.replace(/\s*button\s*/, "")
+    // Assume className only contains "button <key> active"
+    const key = column.className.replace(
+      /\s*(button|active)\s*/g, ""
+    )
     copyToSelected(data, key)
   }
 }
@@ -245,7 +369,9 @@ function playAllClips() {
 
   const playNext = () => {
     index += 1
-    playMark.style.top = `${index * 2.4}em`
+    const { scrollTop } = predefined
+    const top = (index * JUMP) - scrollTop
+    playMark.style.top = `${top}px`
 
     clip = timings.shift()
     playClip(url, clip)
@@ -304,6 +430,30 @@ function updateSettings() {
   const is = getTimes(using, timingData, mid)
 
   was.replaceWith(is)
+}
+
+
+function tweakNext({ target }) {
+  const { id } = target.closest("[id]")
+
+  // << HACK to get the right line number
+  const direction = (id === "down") * 2
+  const rows = Array.from(predefined.children)
+
+  const line = Math.max(
+    2, 
+    Math.min(
+      index + direction,
+      rows.length + 1
+    )
+  ) - 1
+  // HACK >> 
+
+  if (line === index) { return }
+
+  const button = rows[line - 1].querySelector(".button")
+
+  selectTime({ target: button })
 }
 
 
